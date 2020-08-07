e991153231ba89f5e5034e92c051fee2fb8538b3
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-08-07 15:18:25 +0100

    wrap every it......do in a describe

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,22 +16,21 @@ RSpec.describe Post, type: :model do
       should validate_length_of(:content).is_at_most(1000)
         .with_long_message('1000 characters in post is the maximum allowed.')
     }
-  end
 
-  it 'is valid when all attributes are valid' do
-    expect(subject).to be_valid
-  end
+    it 'is valid when all attributes are valid' do
+      expect(subject).to be_valid
+    end
 
-  it 'is invalid when content is blank' do
-    subject.content = nil
-    expect(subject).to_not be_valid
-  end
+    it 'is invalid when content is blank' do
+      subject.content = nil
+      expect(subject).to_not be_valid
+    end
 
-  it 'is invalid when content length is greater than 1000' do
-    subject.content = 'c' * 1001
-    expect(subject).to_not be_valid
+    it 'is invalid when content length is greater than 1000' do
+      subject.content = 'c' * 1001
+      expect(subject).to_not be_valid
+    end
   end
-
   describe 'Associations' do
     it { should belong_to(:user) }
     it { should have_many(:comments).dependent(:destroy) }
